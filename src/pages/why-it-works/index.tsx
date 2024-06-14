6293a7a6269793ad630883c15f5c864534e56116
Alexandre <alexandre.acustica@gmail.com>
2024-06-14 17:42:12 -0300

    pseudo-carrossel-version

--- src/pages/why-it-works/index.tsx
+++ src/pages/why-it-works/index.tsx
@@ -4,29 +4,36 @@ import { Section } from "../../components";
 export const WhyItWorks = () => {
   return (
     <Section id="como-funciona-section">
-      <Grid container spacing={2}>
-        <Grid item xs={0.5}></Grid>
-        <Grid item xs={11}>
-          <Grid container>
-            <Grid item xs={6}>
-              <Grid container>
-                <Grid item xs={12}>
-                  <Typography
-                    sx={{
-                      color: "#00296D",
-                      fontSize: "40px",
-                      fontWeight: "bold",
-                    }}
-                  >
-                    Como funciona?
-                  </Typography>
-                </Grid>
-              </Grid>
-            </Grid>
-            <Grid item xs={6}></Grid>
-          </Grid>
+      <Grid container spacing={4}>
+        <Grid
+          xs={6}
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <Typography
+            sx={{
+              color: "#00296D",
+              fontSize: "40px",
+              fontWeight: "bold",
+            }}
+          >
+            Como funciona?
+          </Typography>
+          <Typography
+            sx={{
+              color: "#00296D",
+              fontSize: "40px",
+              fontWeight: "bold",
+            }}
+          >
+            Entre em contato pelo Whatsapp
+          </Typography>
         </Grid>
-        <Grid item xs={0.5}></Grid>
+        <Grid xs={6}></Grid>
       </Grid>
     </Section>
   );
