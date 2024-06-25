4032fc1b04a74c37a9e5524ba3f5b00545d67ef2
Alexandre Mota <alexandre.acustica@gmail.com>
2024-06-25 18:24:10 -0300

    perguntas Frequentes

--- src/pages/perguntas-frequentes/index.tsx
+++ src/pages/perguntas-frequentes/index.tsx
@@ -7,44 +7,8 @@ import {
 } from "@mui/material";
 import { BigText, Section, Title } from "../../components";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import { perguntasFrequentes } from "../../constantes/perguntasFrequentes";
 
-const perguntasFrequentes = [
-  {
-    title: "Como  saber quando preciso iniciar um processo terapêutico?",
-    resposta:
-      "Lorem ipsum dolor sit amet consectetur adipiscing elit conubia, massa facilisi lectus ullamcorper ridiculus potenti iaculis nibh, interdum faucibus ad diam vivamus vehicula fames. Convallis ornare praesent mollis at habitasse morbi eu ridiculus, tristique proin venenatis dictum nisl volutpat interdum, eleifend velit erat mi penatibus nibh curabitur. Vestibulum augue condimentum luctus auctor nullam imperdiet magna, mattis scelerisque erat odio aliquam risus nam morbi, accumsan dapibus montes facilisis suspendisse rhoncus.",
-  },
-  {
-    title: "Quais os benefícios da Psicoterapia?",
-    resposta:
-      "Lorem ipsum dolor sit amet consectetur adipiscing elit conubia, massa facilisi lectus ullamcorper ridiculus potenti iaculis nibh, interdum faucibus ad diam vivamus vehicula fames. Convallis ornare praesent mollis at habitasse morbi eu ridiculus, tristique proin venenatis dictum nisl volutpat interdum, eleifend velit erat mi penatibus nibh curabitur. Vestibulum augue condimentum luctus auctor nullam imperdiet magna, mattis scelerisque erat odio aliquam risus nam morbi, accumsan dapibus montes facilisis suspendisse rhoncus.",
-  },
-  {
-    title: "O Psicólogo irá resolver meu problema?",
-    resposta:
-      "Lorem ipsum dolor sit amet consectetur adipiscing elit conubia, massa facilisi lectus ullamcorper ridiculus potenti iaculis nibh, interdum faucibus ad diam vivamus vehicula fames. Convallis ornare praesent mollis at habitasse morbi eu ridiculus, tristique proin venenatis dictum nisl volutpat interdum, eleifend velit erat mi penatibus nibh curabitur. Vestibulum augue condimentum luctus auctor nullam imperdiet magna, mattis scelerisque erat odio aliquam risus nam morbi, accumsan dapibus montes facilisis suspendisse rhoncus.",
-  },
-  {
-    title: "A terapia online traz bons resultados?",
-    resposta:
-      "Lorem ipsum dolor sit amet consectetur adipiscing elit conubia, massa facilisi lectus ullamcorper ridiculus potenti iaculis nibh, interdum faucibus ad diam vivamus vehicula fames. Convallis ornare praesent mollis at habitasse morbi eu ridiculus, tristique proin venenatis dictum nisl volutpat interdum, eleifend velit erat mi penatibus nibh curabitur. Vestibulum augue condimentum luctus auctor nullam imperdiet magna, mattis scelerisque erat odio aliquam risus nam morbi, accumsan dapibus montes facilisis suspendisse rhoncus.",
-  },
-  {
-    title: "Quantas sessões irei precisar?",
-    resposta:
-      "Lorem ipsum dolor sit amet consectetur adipiscing elit conubia, massa facilisi lectus ullamcorper ridiculus potenti iaculis nibh, interdum faucibus ad diam vivamus vehicula fames. Convallis ornare praesent mollis at habitasse morbi eu ridiculus, tristique proin venenatis dictum nisl volutpat interdum, eleifend velit erat mi penatibus nibh curabitur. Vestibulum augue condimentum luctus auctor nullam imperdiet magna, mattis scelerisque erat odio aliquam risus nam morbi, accumsan dapibus montes facilisis suspendisse rhoncus.",
-  },
-  {
-    title: "A ansiedade já faz parte da minha vida, isso tem jeito?",
-    resposta:
-      "Lorem ipsum dolor sit amet consectetur adipiscing elit conubia, massa facilisi lectus ullamcorper ridiculus potenti iaculis nibh, interdum faucibus ad diam vivamus vehicula fames. Convallis ornare praesent mollis at habitasse morbi eu ridiculus, tristique proin venenatis dictum nisl volutpat interdum, eleifend velit erat mi penatibus nibh curabitur. Vestibulum augue condimentum luctus auctor nullam imperdiet magna, mattis scelerisque erat odio aliquam risus nam morbi, accumsan dapibus montes facilisis suspendisse rhoncus.",
-  },
-  {
-    title: "Minha vida não faz mais sentido, porque devo fazer psicoterapia?",
-    resposta:
-      "Lorem ipsum dolor sit amet consectetur adipiscing elit conubia, massa facilisi lectus ullamcorper ridiculus potenti iaculis nibh, interdum faucibus ad diam vivamus vehicula fames. Convallis ornare praesent mollis at habitasse morbi eu ridiculus, tristique proin venenatis dictum nisl volutpat interdum, eleifend velit erat mi penatibus nibh curabitur. Vestibulum augue condimentum luctus auctor nullam imperdiet magna, mattis scelerisque erat odio aliquam risus nam morbi, accumsan dapibus montes facilisis suspendisse rhoncus.",
-  },
-];
 export const PerguntasFrequentes = () => {
   return (
     <Section id="duvidas-section">
