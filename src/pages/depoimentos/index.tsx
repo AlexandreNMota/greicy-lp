42e87fbab1764a7ee189ada2c5899a89b2de3d04
Alexandre Mota <alexandre.acustica@gmail.com>
2024-06-27 17:40:21 -0300

    infinite card

--- src/pages/depoimentos/index.tsx
+++ src/pages/depoimentos/index.tsx
@@ -8,51 +8,63 @@ import {
   Typography,
 } from "@mui/material";
 import { BigText, Section, Title } from "../../components";
-import Carousel from "react-material-ui-carousel";
-import { motion, AnimatePresence } from "framer-motion";
+import { motion, useMotionValue, animate } from "framer-motion";
+import useMeasure from "react-use-measure";
+import { useEffect } from "react";
+
+const MotionStack = motion(Stack);
 
 const tutorialSteps = [
   {
-    step: [
-      {
-        nome: "Matias Gomez",
-        stars: 4,
-        desc: "Lorem ipsum dolor sit amet consectetur adipiscing elit conubia, massa facilisi lectus ullamcorper ridiculus potenti iaculis nibh, interdum faucibus ad diam vivamus vehicula fames.",
-      },
-      {
-        nome: "Maria Paz",
-        stars: 3,
-        desc: "Lorem ipsum dolor sit amet consectetur adipiscing elit conubia, massa facilisi lectus ullamcorper ridiculus potenti iaculis nibh, interdum faucibus ad diam vivamus vehicula fames.",
-      },
-      {
-        nome: "Silvia Rios",
-        stars: 3,
-        desc: "Lorem ipsum dolor sit amet consectetur adipiscing elit conubia, massa facilisi lectus ullamcorper ridiculus potenti iaculis nibh, interdum faucibus ad diam vivamus vehicula fames.",
-      },
-    ],
+    nome: "Matias Gomez",
+    stars: 4,
+    desc: "Lorem ipsum dolor sit amet consectetur adipiscing elit conubia, massa facilisi lectus ullamcorper ridiculus potenti iaculis nibh, interdum faucibus ad diam vivamus vehicula fames.",
   },
   {
-    step: [
-      {
-        nome: "Matias Gomez 2",
-        stars: 4,
-        desc: "Lorem ipsum dolor sit amet consectetur adipiscing elit conubia, massa facilisi lectus ullamcorper ridiculus potenti iaculis nibh, interdum faucibus ad diam vivamus vehicula fames.",
-      },
-      {
-        nome: "Maria Paz 2",
-        stars: 3,
-        desc: "Lorem ipsum dolor sit amet consectetur adipiscing elit conubia, massa facilisi lectus ullamcorper ridiculus potenti iaculis nibh, interdum faucibus ad diam vivamus vehicula fames.",
-      },
-      {
-        nome: "Silvia Rios 2",
-        stars: 3,
-        desc: "Lorem ipsum dolor sit amet consectetur adipiscing elit conubia, massa facilisi lectus ullamcorper ridiculus potenti iaculis nibh, interdum faucibus ad diam vivamus vehicula fames.",
-      },
-    ],
+    nome: "Maria Paz",
+    stars: 3,
+    desc: "Lorem ipsum dolor sit amet consectetur adipiscing elit conubia, massa facilisi lectus ullamcorper ridiculus potenti iaculis nibh, interdum faucibus ad diam vivamus vehicula fames.",
+  },
+  {
+    nome: "Silvia Rios",
+    stars: 3,
+    desc: "Lorem ipsum dolor sit amet consectetur adipiscing elit conubia, massa facilisi lectus ullamcorper ridiculus potenti iaculis nibh, interdum faucibus ad diam vivamus vehicula fames.",
+  },
+  {
+    nome: "Matias Gomez 2",
+    stars: 4,
+    desc: "Lorem ipsum dolor sit amet consectetur adipiscing elit conubia, massa facilisi lectus ullamcorper ridiculus potenti iaculis nibh, interdum faucibus ad diam vivamus vehicula fames.",
+  },
+  {
+    nome: "Maria Paz 2",
+    stars: 3,
+    desc: "Lorem ipsum dolor sit amet consectetur adipiscing elit conubia, massa facilisi lectus ullamcorper ridiculus potenti iaculis nibh, interdum faucibus ad diam vivamus vehicula fames.",
+  },
+  {
+    nome: "Silvia Rios 2",
+    stars: 3,
+    desc: "Lorem ipsum dolor sit amet consectetur adipiscing elit conubia, massa facilisi lectus ullamcorper ridiculus potenti iaculis nibh, interdum faucibus ad diam vivamus vehicula fames.",
   },
 ];
 
 export const Depoimentos = () => {
+  let [ref, { width }] = useMeasure();
+  const xTranslation = useMotionValue(0);
+
+  useEffect(() => {
+    const finalPosition = -width / 2 - 8;
+
+    const controls = animate(xTranslation, [0, finalPosition], {
+      ease: "linear",
+      duration: 50,
+      repeat: Infinity,
+      repeatType: "loop",
+      repeatDelay: 0,
+    });
+
+    return controls.stop;
+  }, [xTranslation, width]);
+
   return (
     <Section id="section-depoimentos">
       <Box
@@ -75,65 +87,61 @@ export const Depoimentos = () => {
       >
         <BigText text="Depoimentos" hasMarginBottom={true} />
       </Box>
-      <Carousel
-        sx={{ width: "85%", margin: "auto", marginTop: "50px" }}
-        animation="slide"
-        indicators={true}
-        autoPlay={true}
-        // navButtonsAlwaysVisible
+      <MotionStack
+        direction="row"
+        spacing={4}
+        ref={ref}
+        style={{
+          x: xTranslation,
+          width: "200vw",
+          marginTop: "50px",
+        }}
       >
-        {tutorialSteps.map((item, i) => (
-          <AnimatePresence key={i}>
-            <motion.div
-              initial={{ opacity: 0, x: 100 }}
-              animate={{ opacity: 1, x: 0 }}
-              exit={{ opacity: 0, x: -100 }}
-              transition={{ duration: 5 }}
-            >
-              <Stack direction="row" spacing={4}>
-                {item.step.map((subItem, index) => (
-                  <Card key={index}>
-                    <CardActionArea>
-                      <Typography
-                        className="source-sans-3"
-                        sx={{
-                          textAlign: "initial",
-                          color: "#000",
-                          fontSize: "20px",
-                          fontWeight: "400 !important",
-                          display: "flex",
-                          width: "100%",
-                          justifyContent: "space-around",
-                        }}
-                      >
-                        {subItem.nome}
-                        <Rating
-                          size="small"
-                          name="read-only"
-                          value={subItem.stars}
-                          readOnly
-                        />
-                      </Typography>
-                      <CardContent>
-                        <Typography
-                          className="source-sans-3"
-                          sx={{
-                            color: "#666666",
-                            fontSize: "16px",
-                            textAlign: "justify",
-                          }}
-                        >
-                          {subItem.desc}
-                        </Typography>
-                      </CardContent>
-                    </CardActionArea>
-                  </Card>
-                ))}
-              </Stack>
-            </motion.div>
-          </AnimatePresence>
+        {[...tutorialSteps].map((item, index) => (
+          <Card
+            key={index}
+            sx={{ boxShadow: "rgba(0, 0, 0, 0.35) 0px 5px 15px" }}
+          >
+            <CardActionArea>
+              <Typography
+                className="source-sans-3"
+                sx={{
+                  textAlign: "initial !important",
+                  color: "#000 !important",
+                  fontSize: "20px !important",
+                  fontWeight: "400 !important",
+                  display: "flex !important",
+                  width: "100% !important",
+                  justifyContent: "space-around !important",
+                  borderRadius: "20px !important",
+                  padding: "20px !important",
+                }}
+              >
+                {item.nome}
+                <Rating
+                  size="small"
+                  name="read-only"
+                  value={item.stars}
+                  readOnly
+                  sx={{ marginLeft: "10px" }}
+                />
+              </Typography>
+              <CardContent>
+                <Typography
+                  className="source-sans-3"
+                  sx={{
+                    color: "#666666 !important",
+                    fontSize: "16px !important",
+                    textAlign: "justify !important",
+                  }}
+                >
+                  {item.desc}
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </Card>
         ))}
-      </Carousel>
+      </MotionStack>
     </Section>
   );
 };
