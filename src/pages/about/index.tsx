d09caa1994667b9c004a26184e9e70b37b135867
Alexandre <alexandre.acustica@gmail.com>
2024-07-21 10:25:31 -0300

    Remove unused code and clean up formatting
    The commit includes the removal of unused imports and commented-out code, with some additional formatting updates carried out for cleaner, more readable code. This work has been undertaken across multiple components, helping to maintain a clean and efficient codebase.

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,87 +1,78 @@
-import { Box, Grid, Link, Typography } from "@mui/material";
-import {
-  BigText,
-  CustomText,
-  GridContainer,
-  GridItem,
-  ImageContainer,
-  Section,
-  Title,
-} from "../../components";
+import {Link, Typography} from "@mui/material";
+import {BigText, GridContainer, GridItem, ImageContainer, Section, Title,} from "../../components";
 import anjo from "../../assets/greicy-anjo.png";
-import WhatsAppIcon from "@mui/icons-material/WhatsApp";
-import { StyledButton } from "../../components/UI/header/styles";
 import React from "react";
-import { sobre } from "../../constantes/sobre";
+import {sobre} from "../../constantes/sobre";
 import teste from "../../../public/images/teste.svg";
 import useAppContext from "../../context/app/useAppContext";
+
 export const About = () => {
-  const { isMobile } = useAppContext();
-  return (
-    <Section id="sobre-section">
-      {!isMobile && (
-        <Title title="Sobre mim" sx={{ width: "100%", textAlign: "center" }} />
-      )}
+    const {isMobile} = useAppContext();
+    return (
+        <Section id="sobre-section">
+            {!isMobile && (
+                <Title title="Sobre mim" sx={{width: "100%", textAlign: "center"}}/>
+            )}
 
-      <GridContainer>
-        <GridItem directionFlex="column" flexStart={isMobile} xs={12} md={6}>
-          {!isMobile && <ImageContainer src={anjo} classe="anjo" />}
+            <GridContainer>
+                <GridItem directionFlex="column" flexStart={isMobile} xs={12} md={6}>
+                    {!isMobile && <ImageContainer src={anjo} classe="anjo"/>}
 
-          {isMobile && <img src={teste} width="100%" />}
-          {isMobile && (
-            <Title
-              title="Sobre mim"
-              sx={{ width: "100%", textAlign: "left", marginLeft: "20px" }}
-            />
-          )}
-          {!isMobile && (
-            <StyledButton
-              className="poppins"
-              onClick={() => {}}
-              startIcon={<WhatsAppIcon />}
-              disableRipple
-              sx={{ marginTop: "50px" }}
-            >
-              Agendar Consulta
-            </StyledButton>
-          )}
-        </GridItem>
-        <GridItem
-          unflex={true}
-          xs={12}
-          md={6}
-          padded={!isMobile}
-          customClass="is-about"
-        >
-          <BigText text="Dra Greicy Pais" hasMarginBottom={true} />
-          {sobre.map((item, index) => (
-            <React.Fragment key={index}>
-              {/* <CustomText text={item.texto} classe="sobre-a-greicy" /> */}
-              <Typography className="about-greicy-text">
-                {item.texto}
-              </Typography>
-            </React.Fragment>
-          ))}
-          {!isMobile && (
-            <Link sx={{ color: "#44C6E9", cursor: "pointer" }}>
-              <Typography
-                // className="source-sans-3"
-                sx={{
-                  fontSize: {
-                    xs: "12px", // font-size for extra-small devices
-                    sm: "14px", // font-size for small devices
-                    md: "16px", // font-size for medium devices
-                    lg: "18px", // font-size for large devices
-                    xl: "20px", // font-size for extra-large devices
-                  },
-                }}
-              >
-                {"Currículo Completo>"}
-              </Typography>
-            </Link>
-          )}
-        </GridItem>
-      </GridContainer>
-    </Section>
-  );
+                    {isMobile && <img src={teste} width="100%"/>}
+                    {isMobile && (
+                        <Title
+                            title="Sobre mim"
+                            sx={{width: "100%", textAlign: "left", marginLeft: "20px"}}
+                        />
+                    )}
+                    {/*{!isMobile && (*/}
+                    {/*  <StyledButton*/}
+                    {/*    className="poppins"*/}
+                    {/*    onClick={() => {}}*/}
+                    {/*    startIcon={<WhatsAppIcon />}*/}
+                    {/*    disableRipple*/}
+                    {/*    sx={{ marginTop: "50px" }}*/}
+                    {/*  >*/}
+                    {/*    Agendar Consulta*/}
+                    {/*  </StyledButton>*/}
+                    {/*)}*/}
+                </GridItem>
+                <GridItem
+                    unflex={true}
+                    xs={12}
+                    md={6}
+                    padded={!isMobile}
+                    customClass="is-about"
+                >
+                    <BigText text="Dra Greicy Pais" hasMarginBottom={true}/>
+                    {sobre.map((item, index) => (
+                        <React.Fragment key={index}>
+                            {/* <CustomText text={item.texto} classe="sobre-a-greicy" /> */}
+                            <Typography className="about-greicy-text">
+                                {item.texto}
+                            </Typography>
+                        </React.Fragment>
+                    ))}
+                    {!isMobile && (
+                        <Link sx={{color: "#44C6E9", cursor: "pointer"}}>
+                            <Typography
+                                // className="source-sans-3"
+                                sx={{
+                                    fontSize: {
+                                        xs: "12px", // font-size for extra-small devices
+                                        sm: "14px", // font-size for small devices
+                                        md: "16px", // font-size for medium devices
+                                        lg: "18px", // font-size for large devices
+                                        xl: "20px", // font-size for extra-large devices
+                                    },
+                                }}
+                            >
+                                {"Currículo Completo>"}
+                            </Typography>
+                        </Link>
+                    )}
+                </GridItem>
+            </GridContainer>
+        </Section>
+    );
 };
--- src/pages/depoimentos/index.tsx
+++ src/pages/depoimentos/index.tsx
@@ -1,141 +1,133 @@
-import {
-  Box,
-  Button,
-  Card,
-  CardActionArea,
-  CardContent,
-  Rating,
-  Stack,
-  Typography,
-} from "@mui/material";
-import { BigText, Section, Title } from "../../components";
-import { motion, useMotionValue, animate } from "framer-motion";
+import {Box, Card, CardActionArea, CardContent, Rating, Stack, Typography,} from "@mui/material";
+import {BigText, Section, Title} from "../../components";
+import {animate, motion, useMotionValue} from "framer-motion";
 import useMeasure from "react-use-measure";
-import { useEffect } from "react";
-import { tutorialSteps } from "../../constantes/depoimentos";
+import {useEffect} from "react";
+import {tutorialSteps} from "../../constantes/depoimentos";
+
 const MotionStack = motion(Stack);
 
 export const Depoimentos = () => {
-  let [ref, { width }] = useMeasure();
-  const xTranslation = useMotionValue(0);
+    const [ref, {width}] = useMeasure();
+    const xTranslation = useMotionValue(0);
 
-  useEffect(() => {
-    const finalPosition = -width / 2 - 8;
+    useEffect(() => {
+        const finalPosition = -width / 2 - 8;
 
-    const controls = animate(xTranslation, [0, finalPosition], {
-      ease: "linear",
-      duration: 50,
-      repeat: Infinity,
-      repeatType: "loop",
-      repeatDelay: 0,
-    });
+        const controls = animate(xTranslation, [0, finalPosition], {
+            ease: "linear",
+            duration: 50,
+            repeat: Infinity,
+            repeatType: "loop",
+            repeatDelay: 0,
+        });
 
-    return controls.stop;
-  }, [xTranslation, width]);
+        return controls.stop;
+    }, [xTranslation, width]);
 
-  return (
-    <Section id="section-depoimentos">
-      <Box
-        sx={{
-          width: "100%",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <Title title="Meus Clientes" />
-      </Box>
-      <Box
-        sx={{
-          width: "100%",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <BigText text="Depoimentos" hasMarginBottom={true} />
-      </Box>
-      <MotionStack
-        direction="row"
-        spacing={4}
-        ref={ref}
-        style={{
-          x: xTranslation,
-          width: "200vw",
-          // marginTop: "50px",
-          paddingBottom: "50px",
-        }}
-      >
-        {[...tutorialSteps].map((item, index) => (
-          <Card
-            key={index}
-            sx={{
-              boxShadow: "rgba(0, 0, 0, 0.35) 0px 5px 15px",
-              minHeight: {
-                xs: "100%",
-              },
-            }}
-          >
-            <CardActionArea>
-              <Typography
-                className="source-sans-3"
+    return (
+        <Section id="section-depoimentos">
+            <Box
                 sx={{
-                  textAlign: "initial !important",
-                  color: "#000 !important",
-
-                  fontWeight: "400 !important",
-                  display: "flex !important",
-                  width: "100% !important",
-                  justifyContent: "space-around !important",
-                  borderRadius: "20px !important",
-                  padding: "5px !important",
-                  fontSize: {
-                    xs: "1rem !important",
-                    md: "1.75rem !important",
-                    lg: "2.25rem !important",
-                  },
+                    width: "100%",
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
                 }}
-              >
-                {item.nome}
-                <Rating
-                  size="small"
-                  name="read-only"
-                  value={item.stars}
-                  readOnly
-                  sx={{ marginLeft: { md: "10px" } }}
-                />
-              </Typography>
-              <CardContent>
-                <Typography
-                  className="content-depo"
-                  sx={{
-                    color: "#666666 !important",
-                    // fontSize: "16px !important",
-                    textAlign: "justify !important",
-                    fontSize: {
-                      xs: ".75rem !important",
-                      sm: "1rem !important",
-                      md: "1.25rem  !important",
-                    },
-                  }}
-                >
-                  {item.desc}
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-          </Card>
-        ))}
-      </MotionStack>
-      <Button
-        className="myWonderfulButton"
-        onClick={() => {
-          navigator.geolocation.getCurrentPosition((location) => {
-            console.log(location);
-          });
-        }}
-      >
-        Show Location
-      </Button>
-    </Section>
-  );
+            >
+                <Title title="Meus Clientes"/>
+            </Box>
+            <Box
+                sx={{
+                    width: "100%",
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                }}
+            >
+                <BigText text="Depoimentos" hasMarginBottom={true}/>
+            </Box>
+            <MotionStack
+                direction="row"
+                spacing={4}
+                ref={ref}
+                style={{
+                    x: xTranslation,
+                    width: "200vw",
+                    // marginTop: "50px",
+                    paddingBottom: "50px",
+                }}
+            >
+                {[...tutorialSteps].map((item, index) => (
+                    <Card
+                        key={index}
+                        sx={{
+                            boxShadow: "rgba(0, 0, 0, 0.35) 0px 5px 15px",
+                            minHeight: {
+                                xs: "100%",
+                            },
+                        }}
+                    >
+                        <CardActionArea>
+                            <Typography
+                                className="source-sans-3"
+                                sx={{
+                                    textAlign: "initial !important",
+                                    color: "#000 !important",
+
+                                    fontWeight: "400 !important",
+                                    display: "flex !important",
+                                    width: "100% !important",
+                                    justifyContent: "space-around !important",
+                                    borderRadius: "20px !important",
+                                    padding: "5px !important",
+                                    fontSize: {
+                                        xs: "1rem !important",
+                                        md: "1.75rem !important",
+                                        lg: "2.25rem !important",
+                                    },
+                                }}
+                            >
+                                {item.nome}
+                                <Rating
+                                    size="small"
+                                    name="read-only"
+                                    value={item.stars}
+                                    readOnly
+                                    sx={{marginLeft: {md: "10px"}}}
+                                />
+                            </Typography>
+                            <CardContent>
+                                <Typography
+                                    className="content-depo"
+                                    sx={{
+                                        color: "#666666 !important",
+                                        // fontSize: "16px !important",
+                                        textAlign: "justify !important",
+                                        fontSize: {
+                                            xs: ".75rem !important",
+                                            sm: "1rem !important",
+                                            md: "1.25rem  !important",
+                                        },
+                                    }}
+                                >
+                                    {item.desc}
+                                </Typography>
+                            </CardContent>
+                        </CardActionArea>
+                    </Card>
+                ))}
+            </MotionStack>
+            {/*<Button*/}
+            {/*  className="myWonderfulButton"*/}
+            {/*  onClick={() => {*/}
+            {/*    navigator.geolocation.getCurrentPosition((location) => {*/}
+            {/*      console.log(location);*/}
+            {/*    });*/}
+            {/*  }}*/}
+            {/*>*/}
+            {/*  Show Location*/}
+            {/*</Button>*/}
+        </Section>
+    );
 };
